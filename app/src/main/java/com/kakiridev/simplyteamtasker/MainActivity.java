097c9d3697058643c81f78e76652984db3c54624
Kamil Głowacki <kamilg.praca@gmail.com>
2018-06-18 01:21:04 +0200

    Created: -Google Login/Logout -Custom Action Bar witch icon and login info
    Next Step:
    -Implements "Create Group" and "Search Group" by "Name of Group" and "Passowrd"

--- app/src/main/java/com/kakiridev/simplyteamtasker/MainActivity.java
+++ app/src/main/java/com/kakiridev/simplyteamtasker/MainActivity.java
@@ -1,13 +1,115 @@
 package com.kakiridev.simplyteamtasker;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textName, textEmail;
+    FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitializeAuth();
+
+        imageView = findViewById(R.id.imageView);
+        textName = findViewById(R.id.textViewName);
+        textEmail = findViewById(R.id.textViewEmail);
+        LogoutButton();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        Glide.with(this)
+                .load(user.getPhotoUrl())
+                .into(imageView);
+
+        textName.setText(user.getDisplayName());
+        textEmail.setText(user.getEmail());
+    }
+
+    private void InitializeAuth(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this , new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                } /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mAuth = FirebaseAuth.getInstance();
+        /**
+         mAuthListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+        }
+        }; **/
+    }
+    public void LogoutButton() {
+        ImageButton buttonLogout = findViewById(R.id.buttonLogout);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+                startActivity_LoginActivity();
+            }
+        });
+    }
+
+    public void startActivity_LoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //if the user is not logged in
+        //opening the login activity
+        if (mAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(Status status) {
+                // textViewUserEmail.setText(" ".toString());
+            }
+        });
     }
 }
